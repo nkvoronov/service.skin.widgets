f73f4041511cf78d0ea20c176ae04b97cd747b23
Nikolay Voronov <nkvoronov@gmail.com>
2020-09-06 19:39:12 +0300

    fixes

--- default.py
+++ default.py
@@ -2,9 +2,9 @@ import sys
 from lib import widgets
 
 if (__name__ == "__main__"):
-    widgets.log("script version %s started" % __addonversion__)
+    widgets.log("script version %s started" % widgets.__addonversion__)
     widgets.Main()
     del widgets.Widgets_Monitor
     del widgets.Widgets_Player
     del widgets.Main
-    widgets.log("script version %s stopped" % __addonversion__)
+    widgets.log("script version %s stopped" % widgets.__addonversion__)
